dc460a45a2d6cea3061e680a6c23aa68f7067d62
hauld <hauluu277@gmail.com>
2025-04-19 23:33:36 +0700

    init source

---
+++ Pi/Pi.Domain/Entities/AuditEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pi.Domain.Entities
+{
+    public class AuditEntity<T> : IAudit
+    {
+        public T Id { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
+        public string UpdatedBy { get; set; }
+        public long CreateById { get; set; }
+        public long UpdateById { get; set; }
+    }
+}
---
+++ Pi/Pi.Domain/Entities/IAudit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pi.Domain.Entities
+{
+    public interface IAudit
+    {
+        string CreatedBy { get; set; }
+        DateTime CreateDate { get; set; }
+        DateTime? UpdateDate { get; set; }
+        string UpdatedBy { get; set; }
+        long CreateById { get; set; }
+        long UpdateById { get; set; }
+    }
+}
